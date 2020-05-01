50a951497859d5ea85603e49538b7c3aa1915e28
Samuel Ye Mei <samuel.y.mei@gmail.com>
2020-05-02 00:00:55 +0800

    define kB in constant.f90 as parameter

--- constant.f90
+++ constant.f90
@@ -1,4 +1,4 @@
 module constant
   use precision_m
-  real(kind=fp_kind) :: kB = 0.0019872041d0 ! in kcal/mol/K
+  real(kind=fp_kind), parameter :: kB = 0.0019872041d0 ! in kcal/mol/K
 end module constant
